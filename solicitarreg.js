d5ad7210d2ec516cc07f39a39f19db557818223a
DelfinDomestico <u201821078@upc.edu.pe>
2022-05-24 09:59:08 -0500

    se elimino la pagina solicitud y sus funcionalidades se implementaron en la pagina de servicios

--- solicitarreg.js
+++ solicitarreg.js
@@ -39,17 +39,52 @@ class CCliente{
         return costo;
     }
 }
+let cliente=null;
 
-//desestructurador
-const desestructurar_servicios=([nombre,costo])=>{
-    console.log(nombre,costo);
-};
-//desestructurador
 // funciones de los botones
+let button_ingresar=document.getElementById("ingresar")
 let button_añadirendodoncia=document.getElementById("añadir-endodoncia");
+let button_añadirortodoncia=document.getElementById("añadir-ortodoncia");
+let button_añadirperidoncia=document.getElementById("añadir-peridoncia");
+let button_añadircirugia=document.getElementById("añadir-cirugia");
+let button_añadircorona=document.getElementById("añadir-corona");
+let button_añadirrehabilitacion=document.getElementById("añadir-rehabilitacion");
 let button_añadirlimpieza=document.getElementById("añadir-limpieza");
 let button_mostrarservicios=document.getElementById("mostrar-servicios");
 let button_archivar=document.getElementById("archivar");
+button_ingresar.onclick=()=>{
+    if(cliente==null){
+        let nombre=prompt('Ingrese su nombre por favor');
+        let edad=prompt('Ingrese su edad por favor');
+        cliente=new CCliente(nombre,edad);
+        while(cliente.es_menor())
+        {
+            alert('No cumple con la edad');
+            nombre=prompt('Ingrese su nombre por favor');
+            edad=prompt('Ingrese su edad por favor');
+            cliente.set_datos(nombre,edad);
+        };
+    }
+    else{
+        alert("Ya existe un usuario logeado");
+    }
+    
+}
+button_añadirrehabilitacion=()=>{
+    cliente.add_servicio({nombre:'Rehabilitacion',costo:60});
+}
+button_añadircirugia.onclick=()=>{
+    cliente.add_servicio({nombre:'Cirugia',costo:140});
+}
+button_añadircorona.onclick=()=>{
+    cliente.add_servicio({nombre:'Corona',costo:110});
+}
+button_añadirperidoncia.onclick=()=>{
+    cliente.add_servicio({nombre:'Peridonsia',costo:80});
+}
+button_añadirortodoncia.onclick=()=>{
+    cliente.add_servicio({nombre:'Ortodonsia', costo:40});
+}
 button_añadirendodoncia.onclick=()=>{cliente.add_servicio({nombre : 'Endodonsia', costo : 50});
 Toastify({
     text: "Endodonsia añadida a la lista de servicios"
@@ -75,16 +110,7 @@ button_archivar.onclick=()=>{
 }
 //fin funciones de botones
 
-let nombre=prompt('Ingrese su nombre por favor');
-let edad=prompt('Ingrese su edad por favor');
-cliente=new CCliente(nombre,edad);
-while(cliente.es_menor())
-{
-    alert('No cumple con la edad');
-    nombre=prompt('Ingrese su nombre por favor');
-    edad=prompt('Ingrese su edad por favor');
-    cliente.set_datos(nombre,edad);
-};
+
 
 
 
